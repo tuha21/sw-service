080bf81eb60754d0da8c427e5b2bff1f04306691
tuha <tuha@sapo.vn>
2023-03-24 17:50:58 +0700

    soka

--- src/main/java/com/example/demo/common/tiktok/feign/TiktokOrderFeign.java
+++ src/main/java/com/example/demo/common/tiktok/feign/TiktokOrderFeign.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Component
 @FeignClient(
         name = "TiktokOrderFeign",
-        url = "${tiktok.apiUrl}",
+        url = "https://open-api.tiktokglobalshop.com",
         configuration = TiktokFeignConfig.class
 )
 public interface TiktokOrderFeign {
